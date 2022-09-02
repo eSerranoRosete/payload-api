cdb940e57d83926381380a827038516283aac51e
Eduardo Serrano <eserrano@Eduardos-MacBook-Air.local>
2022-09-02 01:39:28 -0500

    Changed serverURL

--- payload.config.js
+++ payload.config.js
@@ -3,8 +3,7 @@ import Users from "./collections/Users";
 import Clients from "./collections/Clients";
 
 export default buildConfig({
-  // serverURL: "http://localhost:3000",
-  serverURL: "https://app.inteminer.com",
+  serverURL: "https://payload-api.vercel.app",
   admin: {
     user: Users.slug,
   },
