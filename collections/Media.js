a72e6dfd999c3fc9874715189dfe9b9b6ea416b7
Eduardo Serrano <eserrano@Eduardos-MacBook-Air.local>
2022-09-03 20:44:33 -0500

    Changed path to URL of avatar

--- collections/Media.js
+++ collections/Media.js
@@ -9,11 +9,9 @@ const Media = {
     disableLocalStorage: true,
     s3: {
       bucket: "payloadcms-inteminer",
-      prefix: "images/xyz", // files will be stored in bucket folder images/xyz
-      // prefix: ({ doc }) => `assets/${doc.type}`, // dynamic prefixes are possible too
     },
     adminThumbnail: ({ doc }) =>
-      `https://payloadcms-inteminer.s3.us-east-2.amazonaws.com/images/xyz/${doc.filename}`,
+      `https://payloadcms-inteminer.s3.us-east-2.amazonaws.com/images/${doc.filename}`,
   },
   // create a field to access uploaded files in s3 from payload api
   fields: [
@@ -29,7 +27,7 @@ const Media = {
       hooks: {
         afterRead: [
           ({ data: doc }) =>
-            `https://payloadcms-inteminer.s3.us-east-2.amazonaws.com/images/${doc.type}/${doc.filename}`,
+            `https://payloadcms-inteminer.s3.us-east-2.amazonaws.com/images/${doc.filename}`,
         ],
       },
     },
