4e6f21336d18848321a4d12eca53a154aeec0670
Eduardo Serrano <eserrano@Eduardos-MacBook-Air.local>
2022-09-03 20:22:29 -0500

    Changed all Media Collection to match plugin

--- collections/Media.js
+++ collections/Media.js
@@ -6,9 +6,11 @@ const Media = {
     disableLocalStorage: true,
     s3: {
       bucket: "payloadcms-inteminer",
+      prefix: "images/xyz", // files will be stored in bucket folder images/xyz
+      // prefix: ({ doc }) => `assets/${doc.type}`, // dynamic prefixes are possible too
     },
     adminThumbnail: ({ doc }) =>
-      `https://payloadcms-inteminer.s3.us-east-2.amazonaws.com/${doc.filename}`,
+      `https://payloadcms-inteminer.s3.us-east-2.amazonaws.com/images/xyz/${doc.filename}`,
   },
   // create a field to access uploaded files in s3 from payload api
   fields: [
@@ -18,10 +20,13 @@ const Media = {
       access: {
         create: () => false,
       },
+      admin: {
+        disabled: true,
+      },
       hooks: {
         afterRead: [
           ({ data: doc }) =>
-            `https://payloadcms-inteminer.s3.us-east-2.amazonaws.com/${doc.filename}`,
+            `https://payloadcms-inteminer.s3.us-east-2.amazonaws.com/images/${doc.type}/${doc.filename}`,
         ],
       },
     },
